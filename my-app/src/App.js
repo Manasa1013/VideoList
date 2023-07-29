9ee57a292614d5056d489f6ad332fe46a9572c56
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2023-07-29 14:07:49 +0530

    init : create react app

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App bg-red-900">
+      <h1>VIDELOST</h1>
+      <Routes>
+        <Route path="/" element={<h1>Videolist</h1>}></Route>
+        <Route path="*" element={<h1>Videolist</h1>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
